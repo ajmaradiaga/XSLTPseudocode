df4a0632cfbd74348ed4059ee3b4cc77827d8832
Antonio Maradiaga <antoniomaradiaga@msn.com>
2015-02-05 15:18:05 +1100

    Initial build of XSLT Pseudocode

---
+++ XSLTPseudocode/XSLTPseudocode/XSLTPseudocode.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace XSLTPseudocode
+{
+    partial class XSLTPseudocode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtXSLT = new System.Windows.Forms.TextBox();
+            this.btnGeneratePseudocode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtXSLT
+            // 
+            this.txtXSLT.AcceptsReturn = true;
+            this.txtXSLT.AcceptsTab = true;
+            this.txtXSLT.Location = new System.Drawing.Point(12, 39);
+            this.txtXSLT.Multiline = true;
+            this.txtXSLT.Name = "txtXSLT";
+            this.txtXSLT.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXSLT.Size = new System.Drawing.Size(482, 582);
+            this.txtXSLT.TabIndex = 0;
+            // 
+            // btnGeneratePseudocode
+            // 
+            this.btnGeneratePseudocode.Location = new System.Drawing.Point(510, 597);
+            this.btnGeneratePseudocode.Name = "btnGeneratePseudocode";
+            this.btnGeneratePseudocode.Size = new System.Drawing.Size(184, 23);
+            this.btnGeneratePseudocode.TabIndex = 1;
+            this.btnGeneratePseudocode.Text = "Generate Pseudocode";
+            this.btnGeneratePseudocode.UseVisualStyleBackColor = true;
+            this.btnGeneratePseudocode.Click += new System.EventHandler(this.btnGeneratePseudocode_Click);
+            // 
+            // XSLTPseudocode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 633);
+            this.Controls.Add(this.btnGeneratePseudocode);
+            this.Controls.Add(this.txtXSLT);
+            this.Name = "XSLTPseudocode";
+            this.Text = "XSLT Pseudocode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtXSLT;
+        private System.Windows.Forms.Button btnGeneratePseudocode;
+    }
+}
+
---
+++ XSLTPseudocode/XSLTPseudocode/XSLTPseudocode.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+namespace XSLTPseudocode
+{
+    public partial class XSLTPseudocode : Form
+    {
+        String _prefix = "";
+        
+        public XSLTPseudocode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeneratePseudocode_Click(object sender, EventArgs e)
+        {
+            XmlDocument xsltDoc = new XmlDocument();
+
+            try
+            {
+                
+                xsltDoc.LoadXml(txtXSLT.Text);
+
+                XmlNode firstNode = xsltDoc.FirstChild;
+
+                while(!firstNode.Name.ToLower().Contains("stylesheet")){
+                    firstNode = firstNode.NextSibling;
+                }
+
+                _prefix = firstNode.Prefix + ":";
+
+                Console.WriteLine("XSLT Prefix: " + _prefix);
+
+                this.txtXSLT.Text = processNode(firstNode, 0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private String getTabs(int numTabs) {
+            String tabs = "";
+            
+            for (int i = 0; i < numTabs; i++)
+            {
+                tabs += "\t";
+            }
+
+            return tabs;
+        }
+
+        String[] _nextSiblingSkip = new String[] { "value-of" };
+
+        private String processNode(XmlNode node, int numTabs) {
+            String retVal = "";
+
+            String tabs = getTabs(numTabs);
+
+            retVal += tabs;
+
+            String nodeText = "";
+            String nextSib = "";
+            XmlNode child = node.FirstChild;
+            String nodeName = node.Name.ToLower().Replace(_prefix,"");
+
+            if (child != null)
+            {
+                if (child.NodeType == XmlNodeType.Text)
+                {
+                    nodeText = node.InnerText;
+                }
+            }
+
+            if(node.NextSibling != null)
+                nextSib = node.NextSibling.Name.ToLower().Replace(_prefix, "");
+
+            switch (nodeName)
+            {
+                case "stylesheet":
+                    retVal += processAttributesList(node, nodeName, numTabs + 1) + Environment.NewLine + Environment.NewLine;
+                    break;
+                case "template":
+                    retVal += "Template: " + processAttributes(node, nodeName) + Environment.NewLine;
+                    break;
+                case "element":
+                    retVal += "Create Element with name: " + processAttributes(node, nodeName) + Environment.NewLine;
+                    break;
+                case "apply-templates":
+                    retVal += "Apply Template " + processAttributes(node, nodeName) + Environment.NewLine;
+                    break;
+                case "value-of": 
+                    retVal += processAttributes(node, nodeName) + Environment.NewLine;
+                    break;
+                case "for-each":
+                    retVal += "for each " + processAttributes(node, nodeName) + Environment.NewLine;
+                    break;
+                case "output":
+                    retVal += processAttributesList(node, nodeName, numTabs + 1) + Environment.NewLine;
+                    break;
+                default:
+                    retVal += nodeName + " " + processAttributes(node, nodeName) + " -> ";
+                    if (!_nextSiblingSkip.Contains(nextSib))
+                        retVal += nodeText + Environment.NewLine;
+                    break;
+            }
+
+            for (int i = 0; i < node.ChildNodes.Count; i++)
+            {
+                XmlNode item = node.ChildNodes[i];
+
+                if (item.NodeType == XmlNodeType.Text) {
+                    //retVal += item.InnerText;
+                } else if(item.NodeType == XmlNodeType.EndElement){
+                    break;
+                }
+                else {
+                    retVal += processNode(node.ChildNodes[i], numTabs + 1);
+                }
+            }
+            switch (nodeName)
+            {
+                case "template":
+                    retVal += Environment.NewLine;
+                    break;
+                default:
+                    break;
+            }
+            return retVal;
+        }
+
+        String[] _selectAttribSkip = new String[]{"apply-templates", "value-of", "for-each"};
+
+        private String processAttributes(XmlNode node, String nodeName) { 
+            String attributeString = "";
+
+            XmlNode nameAttribute = null;
+
+            nameAttribute = node.Attributes.GetNamedItem("name");
+
+            if (nameAttribute == null) {
+                nameAttribute = node.Attributes.GetNamedItem("Name");
+            }
+
+            if (nameAttribute != null)
+            {
+                attributeString = nameAttribute.Value + "\t";
+            }
+
+            for (int i = 0; i < node.Attributes.Count; i++)
+            {
+                XmlNode item = node.Attributes[i];
+
+                switch (item.Name.ToLower())
+                {
+                    case "name":
+                        break;
+                    case "match":
+                        attributeString += "match: " + item.Value + "\t";
+                        break;
+                    case "select": case "test":
+                    if(item.Value == "."){
+                        attributeString += " -> node()" + "\t";
+                    }    
+                    else if (_selectAttribSkip.Contains(nodeName))
+                        {
+                            attributeString += " -> " + item.Value + "\t";
+                        }
+                        break;
+                    default:
+                        attributeString += "@" + item.Name.ToLower() + ": " + item.Value + ((i+1 == node.Attributes.Count) ? " " : ", ");
+                        break;
+                }
+            }
+
+            
+
+            return attributeString;
+        }
+
+        private String processAttributesList(XmlNode node, String nodeName, int numTabs)
+        {
+
+            String tabs = getTabs(numTabs);
+            String attributeString = nodeName + ": " + Environment.NewLine;
+
+            for (int i = 0; i < node.Attributes.Count; i++)
+            {
+                XmlNode item = node.Attributes[i];
+
+                switch (item.Name.ToLower())
+                {
+                    default:
+                        attributeString += tabs + "* " + item.Name.Replace("xmlns:","Namespace ") + ": " + item.Value + Environment.NewLine;
+                        break;
+                }
+            }
+
+            return attributeString;
+        }
+    }
+}
